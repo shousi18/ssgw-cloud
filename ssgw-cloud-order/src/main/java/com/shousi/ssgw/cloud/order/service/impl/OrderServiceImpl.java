611801ad29daed3392940746f7b242e55103dd1d
贺羿鼎 <13204855+shou_si@user.noreply.gitee.com>
2025-05-07 20:39:07 +0800

    feat(order):配置负载均衡； refactor(order):修改为访问Nacos服务实例

--- ssgw-cloud-order/src/main/java/com/shousi/ssgw/cloud/order/service/impl/OrderServiceImpl.java
+++ ssgw-cloud-order/src/main/java/com/shousi/ssgw/cloud/order/service/impl/OrderServiceImpl.java
@@ -26,8 +26,12 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public Order findOrderByOrderId(Long orderId) {
         Order order = orderMapper.findOrderByOrderId(orderId);
+//        User user = restTemplate.getForObject(
+//                "http://localhost:10100/api/user/findUserByUserId/" + order.getUserId(),
+//                User.class);
+
         User user = restTemplate.getForObject(
-                "http://localhost:10100/api/user/findUserByUserId/" + order.getUserId(),
+                "http://ssgw-cloud-user/api/user/findUserByUserId/" + order.getUserId(),
                 User.class);
         order.setUser(user);
         return order;
