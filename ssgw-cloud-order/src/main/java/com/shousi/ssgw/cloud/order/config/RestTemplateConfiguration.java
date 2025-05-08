ba3c98fe5031ac5ba2a5cd65d57b0fff7c9bfa1f
贺羿鼎 <13204855+shou_si@user.noreply.gitee.com>
2025-05-08 19:03:28 +0800

    feat(order):自定义负载均衡算法；

--- ssgw-cloud-order/src/main/java/com/shousi/ssgw/cloud/order/config/RestTemplateConfiguration.java
+++ ssgw-cloud-order/src/main/java/com/shousi/ssgw/cloud/order/config/RestTemplateConfiguration.java
@@ -1,11 +1,16 @@
 package com.shousi.ssgw.cloud.order.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClients(value = {
+        @LoadBalancerClient(name = "ssgw-cloud-user", configuration = CustomLoadBalancerConfiguration.class)
+})
 public class RestTemplateConfiguration {
 
     @Bean
